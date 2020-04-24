c73d50f7a8b49218094e5cd3f3d44b2aff32ed36
Grzegorz Baranski <gbaranski19@gmail.com>
2020-04-25 01:43:01 +0200

    rebuilded code, everything is working - first apple doesn't extend snake

--- js/newindex.js
+++ js/newindex.js
@@ -4,34 +4,51 @@ class Part {
         this.y = y;
     }
 }
+
 class Snake {
     constructor() {
         this.parts = []
-        this.expand(100, 100);
+        this.parts.push(new Part(100, 100))
     }
-    expand(x, y) {
+    extend() {
+        const { x, y } = this.parts[this.parts.length - 1]
+        console.log({x,y})
         this.parts.push(new Part(x, y))
     }
     draw(ctx) {
         this.parts.forEach((part, index) => {
             ctx.beginPath();
             ctx.arc(part.x, part.y, 10, 0, Math.PI * 2);
-            let color = '#' + Math.random().toString(16).substr(-6);
+            let color = "#212121"
             ctx.fillStyle = color;
             ctx.fill();
             ctx.closePath();
         })
     }
-    move(xVelocity, yVelocity) {
-        this.parts[0].x = this.parts[0].x + xVelocity;
-        this.parts[0].y = this.parts[0].y + yVelocity;
+    getHeadPosition() {
+        return this.parts[0];
     }
+    move(xVelocity, yVelocity) {
+        const newHead = this.parts[0]
+        newHead.x += xVelocity;
+        newHead.y += yVelocity;
+        if(this.parts.length > 1) {
+            const newParts = this.parts
+                .filter((_, index) => index > 0)
+                .map((_, index) => new Part(this.parts[index].x, this.parts[index].y))
+                this.parts = [newHead, ...newParts];
+        }else{
+            this.parts[0] = newHead;
+        }
+    }
+    
 }
+
 class Apple {
     constructor() {
-        this.spawn()
+        this.respawn()
     }
-    spawn() {
+    respawn() {
         this.x = Math.floor(Math.random() * Math.floor((canvas.width / 1.1) / 10)) * 10;
         this.y = Math.floor(Math.random() * Math.floor((canvas.height / 1.3) / 10)) * 10;
     }
@@ -42,38 +59,34 @@ class Apple {
         ctx.stroke();
         ctx.closePath();
     }
+    getPosistion() {
+        return {
+            x: this.x,
+            y: this.y
+        }
+    }
 }
 class GameState {
     constructor() {
         this.snake = new Snake();
         this.apple = new Apple();
-        console.log("Created class")
-    }
-    dupa() {
-        console.log("dupa")
     }
     draw(ctx) {
-        console.log({ game: this })
         this.snake.draw(ctx);
         this.apple.draw(ctx);
     }
-
-    handleKeyPress(key) {
-        const xVelocity = 10;
-        const yVelocity = -10;
-        switch (key.keyCode) {
-            case 37:
-                this.snake.move(-10, 0);
-                break;
-            case 38:
-                this.snake.move(0 , -10);
-                break;
-            case 39:
-                this.snake.move(10,0);
-                break;
-            case 40:
-                this.snake.move(0, 10);
-                break;
+    move(){
+        this.snake.move(this.dx || 0, this.dy || 0);
+    }
+    changeDirection(dx,dy) {
+        this.dx = dx;
+        this.dy = dy;
+    }
+    checkIfCollideWithApple() {
+        if(Math.abs(this.snake.getHeadPosition().x - this.apple.getPosistion().x) < 20 
+        && Math.abs(this.snake.getHeadPosition().y - this.apple.getPosistion().y) < 20 ) {
+            this.apple.respawn();
+            this.snake.extend();
         }
     }
 };
@@ -82,16 +95,39 @@ class GameState {
 
 const gameState = new GameState();
 window.gameState = gameState;
-gameState.dupa()
+
+function handleKeyPress(key) {
+    switch (key.keyCode) {
+        case 37:
+            gameState.changeDirection(-10, 0);
+            break;
+        case 38:
+            gameState.changeDirection(0 , -10);
+            break;
+        case 39:
+            gameState.changeDirection(10,0);
+            break;
+        case 40:
+            gameState.changeDirection(0, 10);
+            break;
+    }
+}
+
 $(document).ready(function () {
     const canvas = document.getElementById("canvas");
     const ctx = canvas.getContext("2d");
     gameState.draw(ctx);
     document.onkeydown = function (key) {
-        gameState.handleKeyPress(key);
+        handleKeyPress(key);
     }
     setInterval(function () {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
         gameState.draw(ctx);
     }, 10)
+    setInterval(function () {
+        gameState.move();   
+        gameState.checkIfCollideWithApple(); 
+    }, 100)
+    
 
 });
