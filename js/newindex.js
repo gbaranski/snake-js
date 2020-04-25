c3e9ba2590b1d6ebaeda7e1f7b21c4a0f84fe46b
Grzegorz Baranski <gbaranski19@gmail.com>
2020-04-25 13:52:42 +0200

    snake extends properly

--- js/newindex.js
+++ js/newindex.js
@@ -13,16 +13,16 @@ class Snake {
     }
     extend() {
         const { x, y } = this.parts[this.parts.length - 1]
-        console.log({x,y})
+        console.log({ x, y })
         this.parts.push(new Part(x, y))
     }
     draw(ctx) {
         this.parts.forEach((part, index) => {
             ctx.beginPath();
             ctx.arc(part.x, part.y, 10, 0, Math.PI * 2);
-            if(index == 0) {
+            if (index == 0) {
                 ctx.fillStyle = "#fff000";
-            } else{
+            } else {
                 ctx.fillStyle = "#212121";
             }
             ctx.fill();
@@ -33,24 +33,22 @@ class Snake {
         return this.parts[0];
     }
     move(xVelocity, yVelocity) {
-        const newHead = this.parts[0];
+        const newHead = new Part(this.parts[0].x, this.parts[0].y)
         newHead.x += xVelocity;
         newHead.y += yVelocity;
         const newParts = this.parts
-            .filter((_, index) => index > 0)
+            .filter((_, index, arr) => index > 0)
             .map((_, index) => new Part(this.parts[index].x, this.parts[index].y));
-            this.parts = [newHead, ...newParts];
+        this.parts = [newHead, ...newParts];
     }
-    
+
 }
 
 class Apple {
-    constructor() {
-        this.respawn()
-    }
-    respawn() {
-        this.x = Math.floor(Math.random() * Math.floor((canvas.width / 1.1) / 10)) * 10;
-        this.y = Math.floor(Math.random() * Math.floor((canvas.height / 1.3) / 10)) * 10;
+
+    respawn(canvasWidth, canvasHeight) {
+        this.x = Math.floor(Math.random() * Math.floor((canvasWidth / 1.1) / 10)) * 10;
+        this.y = Math.floor(Math.random() * Math.floor((canvasHeight / 1.3) / 10)) * 10;
     }
     draw(ctx) {
         ctx.beginPath();
@@ -66,63 +64,106 @@ class Apple {
         }
     }
 }
+class Canvas {
+    constructor(canvas, ctx, gameState) {
+        this.canvas = canvas
+        this.ctx = ctx
+        this.gameState = gameState
+    }
+    draw() {
+        this.gameState.snake.draw(this.ctx);
+        this.gameState.apple.draw(this.ctx);
+    }
+
+    drawGrid() {
+        for (let x = 0.5; x < this.canvas.width; x += 20) {
+            this.ctx.moveTo(x, 0);
+            this.ctx.lineTo(x, this.canvas.height);
+        }
+
+        for (let y = 0.5; y < this.canvas.height; y += 20) {
+            this.ctx.moveTo(0, y);
+            this.ctx.lineTo(this.canvas.width, y);
+        }
+        this.ctx.strokeStyle = "#ddd";
+        this.ctx.stroke();
+    }
+    clearRect() {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+
+}
 class GameState {
-    constructor() {
+    constructor(canvasWidth, canvasHeight) {
         this.snake = new Snake();
         this.apple = new Apple();
+        this.canvasWidth = canvasWidth;
+        this.canvasHeight = canvasHeight;
+        this.apple.respawn(canvasWidth, canvasHeight);
     }
-    draw(ctx) {
-        this.snake.draw(ctx);
-        this.apple.draw(ctx);
-    }
-    move(){
+
+    move() {
         this.snake.move(this.dx || 0, this.dy || 0);
     }
-    changeDirection(dx,dy) {
+    changeDirection(dx, dy) {
         this.dx = dx;
         this.dy = dy;
     }
     checkIfCollideWithApple() {
-        if(Math.abs(this.snake.getHeadPosition().x - this.apple.getPosistion().x) < 20 
-        && Math.abs(this.snake.getHeadPosition().y - this.apple.getPosistion().y) < 20 ) {
-            this.apple.respawn();
+        if (Math.abs(this.snake.getHeadPosition().x - this.apple.getPosistion().x) < 20
+            && Math.abs(this.snake.getHeadPosition().y - this.apple.getPosistion().y) < 20) {
+            this.apple.respawn(this.canvasWidth, this.canvasHeight);
             this.snake.extend();
         }
     }
-    checkIfCollideWithWall(canvas) {
-        if(this.snake.getHeadPosition().x>=canvas.width) {
+    checkIfCollideWithWall() {
+        if (this.snake.getHeadPosition().x >= this.canvasWidth) {
             return true;
         }
-        if(this.snake.getHeadPosition().x<=0){
+        if (this.snake.getHeadPosition().x <= 0) {
             return true;
         }
-        if(this.snake.getHeadPosition().y>=canvas.height) {
+        if (this.snake.getHeadPosition().y >= this.canvasHeight) {
             return true;
         }
-        if(this.snake.getHeadPosition().y<=0) {
+        if (this.snake.getHeadPosition().y <= 0) {
             return true;
         }
         return false;
     }
-    drawGrid(canvas, ctx) {
-        for (var x = 0.5; x < canvas.width; x += 20) {
-            ctx.moveTo(x, 0);
-            ctx.lineTo(x, canvas.height);
-          }
-          
-          for (var y = 0.5; y < canvas.height; y += 20) {
-            ctx.moveTo(0, y);
-            ctx.lineTo(480, y);
-          }
-          ctx.strokeStyle = "#ddd";
-          ctx.stroke();
-        }
+
 };
 
 
 
-const gameState = new GameState();
-window.gameState = gameState;
+$(document).ready(function () {
+    const canvas = document.getElementById("canvas");
+    const  ctx = canvas.getContext("2d");
+    const gameState = new GameState(canvas.width, canvas.height);
+    const gameCanvas = new Canvas(canvas, ctx, gameState);
+
+    window.gameState = gameState;
+
+    document.onkeydown = function (key) {
+        handleKeyPress(key);
+    }
+    const renderGameInterval = setInterval(function () {
+        gameCanvas.clearRect();
+        gameCanvas.drawGrid();
+        gameCanvas.draw();
+    }, 10)
+    const tickSnakeInterval = setInterval(function () {
+        gameState.move();
+        gameState.checkIfCollideWithApple();
+        if (gameState.checkIfCollideWithWall()) {
+            clearInterval(tickSnakeInterval);
+            clearInterval(renderGameInterval);
+        }
+    }, 100)
+
+
+});
+
 
 function handleKeyPress(key) {
     switch (key.keyCode) {
@@ -130,40 +171,13 @@ function handleKeyPress(key) {
             gameState.changeDirection(-10, 0);
             break;
         case 38:
-            gameState.changeDirection(0 , -10);
+            gameState.changeDirection(0, -10);
             break;
         case 39:
-            gameState.changeDirection(10,0);
+            gameState.changeDirection(10, 0);
             break;
         case 40:
             gameState.changeDirection(0, 10);
             break;
     }
 }
-
-$(document).ready(function () {
-    const canvas = document.getElementById("canvas");
-    const ctx = canvas.getContext("2d");
-    
-    gameState.draw(ctx);
-    document.onkeydown = function (key) {
-        handleKeyPress(key);
-    }
-    const renderGameInterval = setInterval(function () {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        gameState.drawGrid(canvas, ctx);
-        gameState.draw(ctx);
-        
-    }, 10)
-    const tickSnakeInterval = setInterval(function () {
-        gameState.move();   
-        gameState.checkIfCollideWithApple(); 
-        if(gameState.checkIfCollideWithWall(canvas)) {
-            console.log("Snake is dead");
-            clearInterval(tickSnakeInterval);
-            clearInterval(renderGameInterval);
-        }
-    }, 100)
-    
-
-});
